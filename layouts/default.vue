fbe7ac2e8b3363420b4360cacfdc551aa17f96a5
Maisam <maisam.vahidsafa@gmail.com>
2024-07-02 03:01:36 +0330

    Add AppHeader.vue and use in default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
-    <AppAlert />
+    <AppHeader />
     <slot />
     <AppAlert />
   </div>
 </template>
+<script setup lang="ts">
+import AppHeader from "~/components/AppHeader.vue";
+</script>

6b8b7cefb437d8404d167d58bfb1418c79782989
Maisam <maisam.vahidsafa@gmail.com>
2024-07-03 04:51:40 +0330

    Add room page with path param

---
+++ pages/room/[id].vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <h3>This page will be displayed at the /room route.</h3>
+  </section>
+</template>
+<script setup lang="ts">
+const route = useRoute()
+
+console.log(route.path)
+// When accessing /posts/1, route.params.id will be 1
+console.log(route.params.id)
+</script>
